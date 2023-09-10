d05f81d27919cb4fdf3505d71646299cb3b21cc2
MuzaffarKuziev <muzaffarkuziev6@gmail.com>
2023-09-10 20:52:06 +0500

    first commit

---
+++ polls/serializer.py
@@ -0,0 +1,7 @@
+from rest_framework import serializers
+from .models import todoModel
+
+class TodoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=todoModel
+        fields=('__all__')
